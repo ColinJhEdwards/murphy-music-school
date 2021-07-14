28ed41b2218bab8a93103a61e4c1d26a7e403e70
Colin Edwards <ColinJhEdwards@gmail.com>
2021-07-13 20:03:37 -0700

    styled course details page

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { pageAnimation, fade } from "../animations";
 
 const CourseDetail = ({
   id,
@@ -14,28 +15,72 @@ const CourseDetail = ({
   img3,
 }) => {
   return (
-    <section className="course">
-      <div>
-        <h2>{title}</h2>
+    <StyledCourse
+      className="courseSection"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
+      <div className="course">
+        <div>
+          <h1>{title}</h1>
+        </div>
+        <Line></Line>
+        <div className="details">
+          <img src={imgLand} alt={title} />
+          <h2>History of {title}</h2>
+          <p>{history}</p>
+        </div>
+        <div className="details">
+          <img src={img2} alt={title} />
+          <h2>Why {title}?</h2>
+          <p>{header}</p>
+          <p>{why}</p>
+        </div>
+        <div className="details">
+          <img src={img3} alt={title} />
+          <h2>How long will it take to learn?</h2>
+          <p>{howLong}</p>
+        </div>
       </div>
-      <div>
-        <img src={imgLand} alt={title} />
-        <h2>History of {title}</h2>
-        <p>{history}</p>
-      </div>
-      <div>
-        <img src={img2} alt={title} />
-        <h2>Why {title}?</h2>
-        <p>{header}</p>
-        <p>{why}</p>
-      </div>
-      <div>
-        <img src={img3} alt={title} />
-        <h2>How long will it take to learn?</h2>
-        <p>{howLong}</p>
-      </div>
-    </section>
+    </StyledCourse>
   );
 };
+
+const StyledCourse = styled(motion.section)`
+  width: 80%;
+  margin: auto;
+  .course {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h1 {
+      margin: 3rem 0rem;
+    }
+    .details {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin: 4rem 0rem;
+      h2 {
+        align-self: flex-start;
+        margin: 2rem 0rem;
+      }
+    }
+    img {
+      border-radius: 15px;
+    }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 3px;
+  width: 100%;
+  background: green;
+  border-radius: 50px;
+`;
 
 export default CourseDetail;
